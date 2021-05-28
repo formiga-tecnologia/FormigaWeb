8d9aaeffc898e6b5a05beb7950fc3b1455d1cab1
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2021-05-28 01:02:56 -0300

    update and fixed bugs of front end

--- pages/home.js
+++ pages/home.js
@@ -9,7 +9,7 @@ function Home(){
     Container("teste","<h1>Descubra o gigante mundo de desenvolvimento da formiga</h1><br>"+ContentValue+"</div>","<img src='../img/cup-of-coffee-1280537_1920.jpg' width='100%' height='100%'>")
     article("<h1>Criação e desenvolvimento de varias soluções</h1><h4>Nós temos diveras soluções para sua empresa ou pequeno negocio</h4>","IDart1")   
     article("<div id='session1'></div>","art2")
-    Boxes("a","a","Id","session1")
+    Boxes("a","<a href=#>ooooi</a>","Id","session1")
     Boxes("a","a","I2d","session1")
     Boxes("a","a","Id3","session1")
 }
