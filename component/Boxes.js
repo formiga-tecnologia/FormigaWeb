a189558f508c1ac3e02f16490d5906accc1d2b9f
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2021-05-30 15:52:11 -0300

    update

--- component/Boxes.js
+++ component/Boxes.js
@@ -2,13 +2,17 @@ import EngineApp from "../Framework/engine.js"
 
 var eng = new EngineApp()
 
-function Boxes(title,content,IdContent,target){
-    eng.componentsEngine.createComponent(IdContent,renderBoxes(title,content))
+function Boxes(img,title,content,IdContent,target){
+    eng.componentsEngine.createComponent(IdContent,renderBoxes(img,title,content))
     eng.componentsEngine.renderComponent(target,IdContent)
 }
 
-function renderBoxes(title,content){
-    return "<div class='boxes'>"+content+"</div>"
+function renderBoxes(img,title,content){
+    return "<div class='boxes'>"+
+    "<img src='"+img+"' height='120vh' width='120vh'>"+
+    "<br><div class='boxes-title'>"+title+"</div>"+
+    "<div class='boxes-text'>"+content+"</div>"+
+    "</div>"
 }
 
 export default Boxes
--- pages/home.js
+++ pages/home.js
@@ -9,7 +9,7 @@ function Home(){
     Container("teste","<h1>Descubra o gigante mundo de desenvolvimento da formiga</h1><br>"+ContentValue+"</div>","<img src='../img/cup-of-coffee-1280537_1920.jpg' width='100%' height='100%'>")
     article("<h1>Criação e desenvolvimento de varias soluções</h1><h4>Nós temos diveras soluções para sua empresa ou pequeno negocio</h4>","IDart1")   
     article("<div id='session1'></div>","art2")
-    Boxes("a","<a href=#>ooooi</a>","Id","session1")
+    Boxes("Sites completos","Construimos sites completos, desde sites de uma unica pagina e paginas completas com varias paginas.","Id","session1")
     Boxes("a","a","I2d","session1")
     Boxes("a","a","Id3","session1")
 }
